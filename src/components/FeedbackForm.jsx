fec0d79615c9f1518588e2b1d5d280ad750874f6
KartikDang <kartikdang@gmail.com>
2022-08-01 14:26:49 +0530

    few more fixes needed

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,32 +4,32 @@ import Rating from './RatingSelect';
 import Button from './shared/Button';
 import FeedbackContext from "./context/FeedbackContext";
 
-function FeedbackForm(){
-    const [text, setText]=useState('');
-    const [rating, setRating]=useState(10);
-    const [btnDisabled, setBtnDisabled]=useState(true);
-    const [message, setMessage]=useState('');
+function FeedbackForm() {
+    const [text, setText] = useState('');
+    const [rating, setRating] = useState(10);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
 
 
-    const {AddFeedback, feedbackEdit, updateFeedback}=useContext(FeedbackContext);
+    const { AddFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
 
-    useEffect(()=>{
+    useEffect(() => {
         setBtnDisabled(false);
         setText(feedbackEdit.item.text);
         setRating(feedbackEdit.item.rating);
-      }, [feedbackEdit])
+    }, [feedbackEdit])
 
-    const handleTextChange=(e)=>{
-           
-        if(text===''){
+    const handleTextChange = (e) => {
+
+        if (text === '') {
             setBtnDisabled(true);
             setMessage(null);
         }
-        else if(text !== '' && text?.trim().length<=10){
+        else if (text !== '' && text?.trim().length <= 10) {
             setBtnDisabled(true);
             setMessage('Feedback shoul be atleast 10 characters long');
         }
-        else{
+        else {
             setBtnDisabled(false);
             setMessage(null);
         }
@@ -42,36 +42,37 @@ function FeedbackForm(){
 
 
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(text.trim().length>10){
-        const newFeedback ={
-            text,
-            rating
-        }
-        console.log(newFeedback);
-            if(feedbackEdit.edit===true){
+
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+            console.log(newFeedback);
+            if (feedbackEdit.edit === true) {
                 console.log("update1");
                 console.log("Reeeee", feedbackEdit.item.id);
                 updateFeedback(feedbackEdit.item.id, newFeedback)
-            }else{
+            } else {
                 AddFeedback(newFeedback);
                 console.log("create1");
             }
 
-       
-        setText('');
-    }
-    
+
+            setText('');
+        }
+
     }
 
-    return(
+    return (
         <Card>
             <form onSubmit={handleSubmit}>
                 <h2>How would you rate your services with us?</h2>
-                <Rating select={(rating)=>{setRating(rating)}}/>
+                <Rating select={(rating) => { setRating(rating) }} />
                 <div className="input-group">
-                    <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text}/>
+                    <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
                     <Button isDisabled={btnDisabled} type="submit">Send</Button>
                 </div>
             </form>
--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -4,57 +4,66 @@ import { v4 as uuidv4 } from 'uuid';
 const FeedbackContext = createContext();
 
 
-export const FeedbackProvider=({children})=>{
+export const FeedbackProvider = ({ children }) => {
 
-    const [feedback, setFeedback]= useState([{
-        id: 1,
-        text: "This is from context",
-        rating: 4
+  const [feedback, setFeedback] = useState([{
+    id: 1,
+    text: "This is from context",
+    rating: 4
 
-    }])
+  }])
 
-    const[feedbackEdit, setFeedbackEdit]= useState({
-        item:{},
-        edit: false
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  });
+
+  function DeleteFeedback(id) {
+    console.log(id);
+    if (window.confirm("Are you sure you want to delete")) {
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+  }
+
+  function AddFeedback(newfeedback) {
+    newfeedback.id = uuidv4();
+    setFeedback([newfeedback, ...feedback])
+  }
+
+  function updateFeedback(id, updItem) {
+    // console.log("prrr",id);
+    // feedback.map((e) => {
+
+    //   if (e.id === id) {
+    //     console.log("UpdateFeedback");
+        setFeedback([...feedback.filter(item => item.id !== id), updItem]);
+    //   }
+    // })
+
+    setFeedbackEdit({
+      item: {},
+      edit: false
+    })
+    //   setFeedback(feedback.map(item=>item.id===id?{id, ...updItem}:item));
+    //   console.log("updating");
+    //   setFeedbackEdit({
+    //     item:{},
+    //     edit: false
+    // })
+  }
+
+  function editFeedback(item) {
+
+    setFeedbackEdit({
+      item,
+      edit: true
     });
+  }
 
-    function DeleteFeedback(id){
-        console.log(id);
-        if(window.confirm("Are you sure you want to delete")){
-          setFeedback(feedback.filter(item=>item.id !== id))
-        }
-      }
-
-      function AddFeedback(newfeedback){
-        newfeedback.id=uuidv4();
-        setFeedback([ newfeedback, ...feedback])
-      }
-
-      function updateFeedback(id,updItem){
-        // console.log("prrr",id);
-        setFeedback(feedback.map((item=>{
-          
-        })))
-        setFeedback(feedback.map(item=>item.id===id?{id, ...updItem}:item));
-        console.log("updating");
-        setFeedbackEdit({
-          item:{},
-          edit: false
-      })
-      }
-
-      function editFeedback(item){
-        
-        setFeedbackEdit({
-            item,
-            edit: true
-        });
-      }
-
-    return (<FeedbackContext.Provider value={{feedback, DeleteFeedback, AddFeedback, editFeedback, feedbackEdit, updateFeedback}}>
-       {children} 
-    </FeedbackContext.Provider>
-    )
+  return (<FeedbackContext.Provider value={{ feedback, DeleteFeedback, AddFeedback, editFeedback, feedbackEdit, updateFeedback }}>
+    {children}
+  </FeedbackContext.Provider>
+  )
 };
 
 export default FeedbackContext;
